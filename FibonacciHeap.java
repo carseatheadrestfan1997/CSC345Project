50d39b361c7cdfc54eca0588b5d0f0b0c1753dbf
chithieu2601 <147015915+chithieu2601@users.noreply.github.com>
2023-10-31 18:44:19 -0700

    Update FibonacciHeap.java

--- FibonacciHeap.java
+++ FibonacciHeap.java
@@ -32,10 +32,34 @@ public class FibonacciHeap {
 
     }
 
-    public void merge() {
-
+    private static void mergeSort(int[] arr, int[] temp, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, temp, left, mid);
+            mergeSort(arr, temp, mid + 1, right);
+            merge(arr, temp, left, mid, right);
+        }
     }
-
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left, j = mid + 1;
+        int k = left;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+        for (k = left; k <= right; k++) {
+            arr[k] = temp[k];
+        }
+    }
     private void resize() {
 
     }
