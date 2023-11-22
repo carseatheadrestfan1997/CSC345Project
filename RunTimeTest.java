677ff86c31f1d74cf2336824b73957e3be072fd4
Ruslan Bilotkach <125308156+RusBilot@users.noreply.github.com>
2023-11-21 20:14:20 -0700

    update test class

--- RunTimeTest.java
+++ RunTimeTest.java
@@ -11,7 +11,11 @@ class RunTimeTest {
     void setup() {
         h = new FibonacciHeap<>();
     }
-
+	
+	/**
+	 * these testcases demonstrate the worst case runtimes for the delete min operation
+	 */
+	
 	@Test
 	void delteMinWorstEx1() {
 		
@@ -37,6 +41,33 @@ class RunTimeTest {
 		System.out.println("~~~~~~~~~~~~~~~~~~~~");
 		
 	}
+	
+	@Test
+	void delteMinWorstEx3() {
+		
+		for (int i = 0; i < 1000; i++) {
+			h.insert(i + 1, i + 1);
+		}
+		
+		System.out.println("removed minimum priority object: " + h.delete_min().getPriority());
+		System.out.println("performed " + h.mergeCounter + "merge operations on initial size 1000");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~");
+		
+	}
+	
+	@Test
+	void delteMinWorstEx4() {
+		
+		for (int i = 0; i < 10000; i++) {
+			h.insert((i + 1) * 2, (i + 1) * 2) ;
+		}
+		
+		System.out.println("removed minimum priority object: " + h.delete_min().getPriority());
+		System.out.println("performed " + h.mergeCounter + "merge operations on initial size 10000");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~");
+		
+	}
+
 	
 	
 }
