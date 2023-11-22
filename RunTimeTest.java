626a4141b8d6b4ad59eb755a15b38a8985f2b173
Ruslan Bilotkach <125308156+RusBilot@users.noreply.github.com>
2023-11-21 22:31:18 -0700

    further changes

--- RunTimeTest.java
+++ RunTimeTest.java
@@ -222,7 +222,7 @@ class RunTimeTest {
 		
 	}
 	
-	@Test
+	// @Test
 	void decreaseKeyWorst() {
 		
 		for (int i = 0; i < 1000; i++) {
@@ -239,7 +239,7 @@ class RunTimeTest {
 		
 	}
 	
-	@Test
+	// @Test
 	void decreaseKeyWorst2() {
 		
 		for (int i = 0; i < 10000; i++) {
@@ -256,7 +256,7 @@ class RunTimeTest {
 		
 	}
 	
-	@Test
+	// @Test
 	void decreaseKeyWorst3() {
 		
 		for (int i = 0; i < 10000; i++) {
@@ -273,6 +273,30 @@ class RunTimeTest {
 		
 	}
 	
+	// @Test
+	void decreaseKeyWorst4() {
+		
+		Random random = new Random();
+	    
+	    int max = 100000;
+	    int min = 10;
+	    
+		
+		for (int i = 0; i < 100000; i++) {
+			int randomNumber = random.nextInt(max - min + 1) + min;
+	        h.insert(randomNumber, randomNumber);
+		}
+		
+		h.decrease_key(h.lastNode, 9);
+		
+		System.out.println("decrease key worst case example 4:");
+		System.out.println("decreased to top: " + h.lastNode.getPriority());
+		System.out.println("performed " + (h.mergeCounter + h.cutCounter) + " merge and cut operations on initial size 100000");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~");
+		
+		
+	}
+	
 	/**
 	 * change the value of i's upper bound and max / min for different
 	 */
@@ -283,13 +307,13 @@ class RunTimeTest {
 		// Create a Random object to generate random numbers
 	    Random random = new Random();
 	    
-	    int max = 1000000;
+	    int max = 10000;
 	    int min = 10;
 	    
 	    
 	    
 	    // Generate 1000 random integers within the specified range
-	    for (int i = 0; i < 100000; i++) {
+	    for (int i = 0; i < 1001; i++) {
 	        int randomNumber = random.nextInt(max - min + 1) + min;
 	        h.insert(randomNumber, randomNumber);
 	    }
@@ -297,7 +321,81 @@ class RunTimeTest {
 	    h.delete_min();
 	    int dev = (h.mergeCounter + h.cutCounter);
 	    
-	    int misc = 1000;
+	    int misc = 10000;
+	    
+	    while (misc >= h.lastNode.getPriority()) {
+	    	misc = random.nextInt(max - min + 1) + min;;
+	    }
+	    
+		h.decrease_key(h.lastNode, misc);
+		
+		System.out.println("decrease key Best / amortized case example 1:");
+		System.out.println("decreased to top: " + h.lastNode.getPriority());
+		System.out.println("performed " + (h.mergeCounter + h.cutCounter - dev) + " merge and cut operations on initial size 1000");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~");
+		
+	}
+	
+	@Test
+	void decreaseKeyAve2() {
+		
+		// Create a Random object to generate random numbers
+	    Random random = new Random();
+	    
+	    int max = 10000;
+	    int min = 10;
+	    
+	    
+	    
+	    // Generate 1000 random integers within the specified range
+	    for (int i = 0; i < 10001; i++) {
+	        int randomNumber = random.nextInt(max - min + 1) + min;
+	        h.insert(randomNumber, randomNumber);
+	    }
+		
+	    h.delete_min();
+	    int dev = (h.mergeCounter + h.cutCounter);
+	    
+	    int misc = 10000;
+	    
+	    while (misc >= h.lastNode.getPriority()) {
+	    	misc = misc / 6;
+	    }
+	    
+		h.decrease_key(h.lastNode, misc);
+		
+		System.out.println("decrease key Best / amortized case example 2:");
+		System.out.println("decreased to top: " + h.lastNode.getPriority());
+		System.out.println("performed " + (h.mergeCounter + h.cutCounter - dev) + " merge and cut operations on initial size 10000");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~");
+		
+	}
+	
+	@Test
+	void decreaseKeyAve3() {
+		
+		// Create a Random object to generate random numbers
+	    Random random = new Random();
+	    
+	    int max = 100000;
+	    int min = 10;
+	    
+	    
+	    
+	    // Generate 1000 random integers within the specified range
+	    for (int i = 0; i < 100001; i++) {
+	        int randomNumber = random.nextInt(max - min + 1) + min;
+	        h.insert(randomNumber, randomNumber);
+	    }
+		
+	   
+	    
+	    int misc = 9;
+	    
+	    h.decrease_key(h.find_min(), misc);
+	    int dev = (h.mergeCounter + h.cutCounter);
+	    
+	    misc = 100000;
 	    
 	    while (misc >= h.lastNode.getPriority()) {
 	    	misc = random.nextInt(max - min + 1) + min;
@@ -305,13 +403,60 @@ class RunTimeTest {
 	    
 		h.decrease_key(h.lastNode, misc);
 		
-		System.out.println("decrease key Best / amortized case example 1:");
+		System.out.println("decrease key Best / amortized case example 3:");
 		System.out.println("decreased to top: " + h.lastNode.getPriority());
 		System.out.println("performed " + (h.mergeCounter + h.cutCounter - dev) + " merge and cut operations on initial size 100000");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~");
 		
 	}
 	
+	@Test
+	void decreaseKeyAve4() {
+		
+		// Create a Random object to generate random numbers
+	    Random random = new Random();
+	    
+	    int max = 10000;
+	    int min = 10;
+	    
+	    System.out.println("decrease key Best / amortized case example 4:");
+	    
+	    
+	    // Generate 1000 random integers within the specified range
+	    for (int i = 0; i < 10001; i++) {
+	        int randomNumber = random.nextInt(max - min + 1) + min;
+	        h.insert(randomNumber, randomNumber);
+	    }
+		
+	    int misc = 10000;
+	    
+	    while (misc >= h.find_min().getPriority()) {
+	    	misc = misc/2;
+	    }
+	    
+	    h.decrease_key(h.find_min(), misc);
+	    int dev = (h.mergeCounter + h.cutCounter);
+	    
+	    System.out.println("initial decrease key on random data:");
+	    System.out.println("decreased to top: " + h.lastNode.getPriority());
+		System.out.println("performed " + dev + " merge and cut operations on initial size 10000");
+	    
+	    misc = 10000;
+	    
+	    while (misc >= h.lastNode.getPriority()) {
+	    	misc = random.nextInt(max - min + 1) + min;
+	    }
+	    
+	    misc = 9;
+	    
+		h.decrease_key(h.lastNode, misc);
+		
+		System.out.println("second decrease key operation on same set, random data:");
+		System.out.println("decreased to top: " + h.lastNode.getPriority());
+		System.out.println("performed " + (h.mergeCounter + h.cutCounter - dev) + " merge and cut operations on initial size 9999");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~");
+		
+	}
 	
 	
 	
